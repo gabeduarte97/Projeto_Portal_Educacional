ef5ac95a485ab0bf39024a6137864ddbc5089ca0
gabeduarte97 <gabrielduartemorato@hotmail.com>
2025-06-03 20:39:44 -0300

    Delete portal-educacional directory

--- portal-educacional/app/view/contato-conteudo.php
+++
@@ -1,3 +0,0 @@
-<h2>Contato</h2>
-<p>Você pode entrar em contato conosco pelo e-mail: <strong>contato@educacional.com</strong></p>
-<p>Ou visite nosso campus virtual para suporte e dúvidas.</p>
--- portal-educacional/app/view/sobre-conteudo.php
+++
@@ -1,3 +0,0 @@
-<h2>Sobre o Portal</h2>
-<p>Este portal foi criado como parte do curso de Análise e Desenvolvimento de Sistemas. 
-Aqui você encontrará postagens educacionais, comentários de usuários e uma área administrativa.</p>

0be0820eadaa7a880258320bc30d6f7d3df90f1a
gabeduarte97 <gabrielduartemorato@hotmail.com>
2025-06-07 23:53:51 -0300

    Add files via upload

---
+++ portal-educacional/app/core/Seguranca.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Core;
+class Seguranca {
+    public static function gerarToken() {
+        if (empty($_SESSION['csrf_token'])) {
+            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+        }
+        return $_SESSION['csrf_token'];
+    }
+    public static function verificarToken($token) {
+        return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
+    }
+}
---
+++ portal-educacional/app/view/sobre-conteudo.php
@@ -0,0 +1,3 @@
+<h2>Sobre o Portal</h2>
+<p>Este portal foi criado como parte do curso de Análise e Desenvolvimento de Sistemas. 
+Aqui você encontrará postagens educacionais, comentários de usuários e uma área administrativa.</p>
